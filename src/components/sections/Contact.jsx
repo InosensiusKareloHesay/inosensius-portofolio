cbebce9dd302ae2b4b6ff9d201e5a9ea2b738eab
DESKTOP-P3AEBDC\Acer <karelo@elnusa.co.id>
2025-03-14 12:22:35 +0700

    Edit styling in Contact

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -30,10 +30,10 @@ export const Contact = () => {
     
     return (
         <section id="contact"
-            className="min-h-screen flex item-center justify-center py-20"
+            className="min-h-screen flex item-center justify-center relative py-20"
         >
             <RevealOnScroll>
-                <div className="px-4 w-150">
+                <div className="w-full max-w-3xl px-4">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-[#1452a3] to-emerald-400 bg-clip-text text-transparent text-center">
                         Get In Touch
                     </h2>
