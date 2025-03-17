8078868f2a489bdb8b90f4021593afb6932201d3
DESKTOP-P3AEBDC\Acer <karelo@elnusa.co.id>
2025-03-17 09:29:40 +0700

    Edit styling add active n focus in MobileMenu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,6 +1,6 @@
 export const MobileMenu = ( {menuOpen, setMenuOpen} ) => {
     return (
-        <div className={`fixed top-0 left-0 right-0 w-full bg-[#163564]/95 z-40 flex flex-col items-center justify-center
+        <div className={`fixed top-0 left-0 right-0 w-full bg-[#163564] z-40 flex flex-col items-center justify-center
                         transition-all duration-300 ease-in-out
 
                         ${menuOpen 
@@ -19,32 +19,44 @@ export const MobileMenu = ( {menuOpen, setMenuOpen} ) => {
             <a 
                 href="#" 
                 onClick={()=> setMenuOpen(false)}
-                className={`text-2xl font-semibold text-[#c1d8f6] hover:text-emerald-400 my-4 transform transition-transform duration-300
+                className={`text-2xl font-semibold text-[#c1d8f6] my-4 transform transition-transform duration-300
                     ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}
+                    hover:text-emerald-400 hover:-translate-y-2
+                    focus:text-emerald-400 focus:-translate-y-2
+                    active:text-emerald-400 active:-translate-y-2
                 `}>
                     Home
             </a>
             <a 
                 href="#about" 
                 onClick={()=> setMenuOpen(false)}
-                className={`text-2xl font-semibold text-[#c1d8f6] hover:text-emerald-400 my-4 transform transition-transform duration-300
+                className={`text-2xl font-semibold text-[#c1d8f6] my-4 transform transition-transform duration-300
                     ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}
+                    hover:text-emerald-400 hover:-translate-y-2
+                    focus:text-emerald-400 focus:-translate-y-2
+                    active:text-emerald-400 active:-translate-y-2
                 `}>
                     About
             </a>
             <a 
                 href="#projects" 
                 onClick={()=> setMenuOpen(false)}
-                className={`text-2xl font-semibold text-[#c1d8f6] hover:text-emerald-400 my-4 transform transition-transform duration-300
+                className={`text-2xl font-semibold text-[#c1d8f6] my-4 transform transition-transform duration-300
                     ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}
+                    hover:text-emerald-400 hover:-translate-y-2
+                    focus:text-emerald-400 focus:-translate-y-2
+                    active:text-emerald-400 active:-translate-y-2
                 `}>
                     Projects
             </a>
             <a 
                 href="#contact" 
                 onClick={()=> setMenuOpen(false)}
-                className={`text-2xl font-semibold text-[#c1d8f6] hover:text-emerald-400 my-4 transform transition-transform duration-300
+                className={`text-2xl font-semibold text-[#c1d8f6] my-4 transform transition-transform duration-300
                     ${menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"}
+                    hover:text-emerald-400 hover:-translate-y-2
+                    focus:text-emerald-400 focus:-translate-y-2
+                    active:text-emerald-400 active:-translate-y-2
                 `}>
                     Contact
             </a>
